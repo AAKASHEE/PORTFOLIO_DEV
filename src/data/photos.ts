83d05cb0db4e9ccea7ea341041a2230316f32637
AAKASHEE <aakashpatra253@gmail.com>
2024-12-19 00:39:41 +0530

    added img link

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -1,25 +1,25 @@
 export const photos = [
   {
     id: 1,
-    url: "./Images/projects/aakashe_02", // Replace with your photo URL
+    url: ".img/aakashe_02", // Replace with your photo URL
     link: "https://linkedin.com/in/yourusername", // Replace with your LinkedIn or relevant link
     caption: "At the University Library",
   },
   {
     id: 2,
-    url: "./Images/projects/aakashe_04", // Replace with your photo URL
+    url: ".img/aakashe_04", // Replace with your photo URL
     link: "https://github.com/yourusername", // Replace with your GitHub or relevant link
     caption: "With the Cultural Team",
   },
   {
     id: 3,
-    url: "./Images/projects/aakashe_03", // Replace with your photo URL
+    url: ".img/aakashe_03", // Replace with your photo URL
     link: "https://twitter.com/yourusername", // Replace with your Twitter or relevant link
     caption: "Speaking at Tech Conference",
   },
   {
     id: 4,
-    url: "./Images/projects/aakashe_01", // Replace with your photo URL
+    url: ".img/aakashe_01", // Replace with your photo URL
     link: "https://instagram.com/yourusername", // Replace with your Instagram or relevant link
     caption: "Professional Headshot",
   },
