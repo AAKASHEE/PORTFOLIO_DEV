b1da9aa2cd5925891bfc806b500a8b24e10b0db9
AAKASHEE <aakashpatra253@gmail.com>
2025-01-05 02:58:32 +0530

    added a few project overview images

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -1,11 +1,28 @@
 export const projects = [
   {
     title: "AAKASHEE'S NOTES TAKING APP",
-    description: "-AN ELECTRON APPLICATION- REACT AND TYPESCRIPT",
+    description: "AN ELECTRON APPLICATION- REACT AND TYPESCRIPT",
     image: "./img/n_01.png",
 
     tags: ["React", "Typescript", "Utility"],
     link: "https://github.com/AAKASHEE/AAKASHE_PROJECTS_NOTES.git",
   },
+  {
+    title: "DOM MANIPULATION LIBRARY",
+    description: "The JavaScript superpowers for effortlessly bending web pages to your will.",
+    image: "./img/DOM_01.png", "./img/DOM_02.png": "./img/DOM_03.png",
 
+    tags: ["React", "Typescript", "Utility"],
+    link: "https://github.com/AAKASHEE/DOM_MANIPULATION_LIBRARY",
+  },
+  {
+    title: "A full-stack portfolio project built with React, Vite, and Express",
+    description: "This is a full-stack portfolio project built with React, Vite, and Express. The backend connects to a MongoDB database to manage data, and the frontend is deployed using Vercel.",
+    image: "./img/PORTFOLIO.png",
+
+    tags: ["React", "Typescript", "Utility"],
+    link: "https://github.com/AAKASHEE/PORTFOLIO_DEV",
+  }
 ];
+
+
