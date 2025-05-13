e557f92534bc01363161fc6db20a4194cb2b7556
AAKASHEE <aakashpatra253@gmail.com>
2025-05-14 02:19:10 +0530

    updated learnings

--- frontend/src/data/achievements.ts
+++ frontend/src/data/achievements.ts
@@ -2,12 +2,12 @@ export const achievements = [
   {
     title: 'HardCore Problem Solver',
     organization: `GEEKSFORGEEKS@4 ⭐️  
-    Contest Rating: 1870, Global Rank: 1237; 
-    RANK: 19 (DSCE)`,
+    Contest Rating: 1870, Global Rank: 1427; 
+    RANK: 11 (DSCE)`,
     date: 'August 2024'
   },
   {
-    title: 'Critical Problem Analyzer Rank:877,352',
+    title: 'Critical Problem Analyzer Rank:527,962',
     organization: 'LEETCODE',
     date: 'September 2024'
   },
